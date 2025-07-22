44a6db74a292803b2f1ac25973fa6c37fbc834e4
Urs27345 <optik@diepolder.ch>
2025-07-22 07:54:45 -0400

    Update index.tsx

--- src/components/pages/home/HomeContent/index.tsx
+++ src/components/pages/home/HomeContent/index.tsx
@@ -12,6 +12,7 @@ import AirImage from "@/assets/air.jpg";
 import SteelImage from "@/assets/steel.jpg";
 import ElectroImage from "@/assets/electro.jpg";
 import FassadeImage from "@/assets/fassade.jpg";
+
 import styles from "./style.module.scss";
 import Container from "../../../module/container";
 import SliderComponent from "../../../ui/slider";
@@ -26,64 +27,39 @@ type Props = {
 
 const HomeContent: React.FC<Props> = ({ dict }) => {
   const contentList = [
-    {
-      content: <p dangerouslySetInnerHTML={{ __html: dict.beachDescription }} />,
-      image: BeachImage,
-    },
-    {
-      content: <p dangerouslySetInnerHTML={{ __html: dict.swimmingDescription }} />,
-      image: SwimmingPoolImage,
-    },
-    {
-      content: <p dangerouslySetInnerHTML={{ __html: dict.highQuality }} />,
-      image: EpoxiImage,
-    },
-    {
-      content: <p dangerouslySetInnerHTML={{ __html: dict.importedVeneer }} />,
-      image: VeneerImage,
-    },
-    {
-      content: <p dangerouslySetInnerHTML={{ __html: dict.stableSoundProof }} />,
-      image: PuertoImage,
-    },
-    {
-      content: <p dangerouslySetInnerHTML={{ __html: dict.soundproofAluminum }} />,
-      image: WindowImage,
-    },
-    {
-      content: <p dangerouslySetInnerHTML={{ __html: dict.airCondition }} />,
-      image: AirImage,
-    },
-    {
-      content: <p dangerouslySetInnerHTML={{ __html: dict.importedSeismic }} />,
-      image: SteelImage,
-    },
-    {
-      content: <p dangerouslySetInnerHTML={{ __html: dict.electricalSystem }} />,
-      image: ElectroImage,
-    },
-    {
-      content: <p dangerouslySetInnerHTML={{ __html: dict.fachadaVentilada }} />,
-      image: FassadeImage,
-    },
+    { content: <p dangerouslySetInnerHTML={{ __html: dict.beachDescription }} />, image: BeachImage },
+    { content: <p dangerouslySetInnerHTML={{ __html: dict.swimmingDescription }} />, image: SwimmingPoolImage },
+    { content: <p dangerouslySetInnerHTML={{ __html: dict.highQuality }} />, image: EpoxiImage },
+    { content: <p dangerouslySetInnerHTML={{ __html: dict.importedVeneer }} />, image: VeneerImage },
+    { content: <p dangerouslySetInnerHTML={{ __html: dict.stableSoundProof }} />, image: PuertoImage },
+    { content: <p dangerouslySetInnerHTML={{ __html: dict.soundproofAluminum }} />, image: WindowImage },
+    { content: <p dangerouslySetInnerHTML={{ __html: dict.airCondition }} />, image: AirImage },
+    { content: <p dangerouslySetInnerHTML={{ __html: dict.importedSeismic }} />, image: SteelImage },
+    { content: <p dangerouslySetInnerHTML={{ __html: dict.electricalSystem }} />, image: ElectroImage },
+    { content: <p dangerouslySetInnerHTML={{ __html: dict.fachadaVentilada }} />, image: FassadeImage },
   ];
 
   return (
     <Container className={styles.wrapper}>
+      {/* Intro-Text mit neuen CSS-Klassen */}
       <div className="py-4">
-        <p className="mb-5" dangerouslySetInnerHTML={{ __html: dict.intro }} />
-        <p className="mb-8" dangerouslySetInnerHTML={{ __html: dict.intro1 }} />
+        <p className={styles.intro} dangerouslySetInnerHTML={{ __html: dict.intro }} />
+        <p className={styles.intro1} dangerouslySetInnerHTML={{ __html: dict.intro1 }} />
       </div>
+
+      {/* Content-Liste */}
       <div className="flex flex-col gap-14 mb-10">
         {contentList.map((item, index) => (
           <div className="grid grid-cols-2 gap-5" key={index}>
             {item.content}
             <div className="w-full">
-              {!!item.image ? <Image src={item.image} alt="image" className={styles.image} /> : null}
+              {!!item.image && <Image src={item.image} alt="image" className={styles.image} />}
             </div>
           </div>
         ))}
       </div>
+
+      {/* Slider */}
       <SliderComponent>
         <Image src={SliderImage1} alt="01" />
         <Image src={SliderImage2} alt="02" />
