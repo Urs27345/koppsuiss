4b9d2f0e728d4d5dec2652d1b44e0c59c1a11cdc
Rafael <webstar0103@gmail.com>
2025-07-22 21:54:19 +0300

    add: google map

--- src/app/[locale]/(pages)/ventas/page.tsx
+++ src/app/[locale]/(pages)/ventas/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import React from "react";
+import Link from "next/link";
 import Image from "next/image";
 
 import female from "@/assets/santa/female.png";
 import male from "@/assets/santa/man.png";
+import map from "@/assets/map.jpeg";
 
 import { partnerList } from "../../../../utils/constant";
 import Container from "../../../../components/module/container";
@@ -30,6 +32,11 @@ const SantaCruz = () => {
               <p className="text-center text-lg text-secondaryGray font-medium">{item.phone}</p>
             </div>
           ))}
+        </div>
+        <div className={styles.mapWrapper}>
+          <Link href={"https://maps.app.goo.gl/mivYasvf1aFba8yP9"} target="_blank">
+            <Image src={map} alt={"map"} className={styles.image} />
+          </Link>
         </div>
       </div>
     </Container>
