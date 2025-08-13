f5827e51d0505b6e8128d081b02d7b2c1e3f9591
Rafael <webstar0103@gmail.com>
2025-08-13 22:17:26 +0300

    add: schema for seo

--- src/components/module/pageMeta/index.tsx
+++ src/components/module/pageMeta/index.tsx
@@ -46,19 +46,46 @@ const PageMeta: React.FC<PageMetaProps> = ({ title, url, canonical, description,
       <link rel="alternate" href="https://www.koppsuisse.ch/es" hrefLang="es" />
       <link rel="alternate" href="https://www.koppsuisse.ch" hrefLang="x-default" />
 
-      {/* <script
+      <script
+        type="application/ld+json"
         dangerouslySetInnerHTML={{
-          __html: `
-              (function(f,n) {
-                n = document.createElement('script');
-                n.src = 'https://fcdn.answerly.io/fn.js';
-                n.setAttribute('data-companyId', f);
-                document.getElementsByTagName('html')[0].insertAdjacentElement('beforeend', n);
-              })('5f9de045-426e-4c6b-b7e5-bbbfdddfd1e1');
-            `,
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            name: "CROP SUISSE",
+            brand: "Kopp Suisse",
+            url: "https://koppsuisse.ch",
+            logo: "https://koppsuisse.ch/logo.png",
+            foundingLocation: "Santa Cruz, Bolivia",
+            address: {
+              "@type": "PostalAddress",
+              addressLocality: "Santa Cruz de la Sierra",
+              addressRegion: "SCZ",
+              addressCountry: "BO",
+            },
+            sameAs: ["https://www.facebook.com/...", "https://www.instagram.com/...", "https://www.linkedin.com/..."],
+          }),
         }}
       />
-      <script>{`(function(f,n) { n = document.createElement('script'); n.src = 'https://fcdn.answerly.io/fn.js'; n.setAttribute('data-companyId', f); document.getElementsByTagName('html')[0].insertAdjacentElement('beforeend', n); })('5f9de045-426e-4c6b-b7e5-bbbfdddfd1e1');`}</script> */}
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "WebSite",
+            "@id": "https://koppsuisse.ch/",
+            url: "https://koppsuisse.ch",
+            name: "Kopp Suisse",
+            publisher: { "@id": "https://koppsuisse.ch/#org" },
+            potentialAction: {
+              "@type": "SearchAction",
+              target: "https://koppsuisse.ch/search?q={search_term_string}",
+              "query-input": "required name=search_term_string",
+            },
+            inLanguage: ["de", "es", "en"],
+          }),
+        }}
+      />
     </Head>
   );
 };
