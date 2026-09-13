0b857c96f524d0c3e8d1c632f1f160832ee0aa23
Urs27345 <optik@diepolder.ch>
2026-09-13 16:01:04 -0400

    fix(routing): map FranzKopp-7f4k9m2x in locale page router

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,13 @@
 import HeroSection from "../../components/pages/home/hero";
 import HomeContent from "../../components/pages/home/HomeContent";
 import { getDictionary } from "../../lib/getDictionary";
+import FranzKoppPage from "../FranzKopp-7f4k9m2x/page";
 
 export default async function Home({ params }: { params: { locale: string } }) {
+  if (params.locale === "FranzKopp-7f4k9m2x") {
+    return <FranzKoppPage />;
+  }
+
   const dict = await getDictionary(params.locale);
   return (
     <div>
